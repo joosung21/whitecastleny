c6d06c0af80cda7577f451123ebb57d8598ce33f
Jay Ahn <joosung21@gmail.com>
2024-04-14 12:04:29 +0900

    모바일 아이콘 변경

--- src/app/components/Layout/Left/components/NavLinks.tsx
+++ src/app/components/Layout/Left/components/NavLinks.tsx
@@ -4,6 +4,7 @@ import {
   HomeIcon,
   ShoppingBagIcon,
   TicketIcon,
+  BuildingOfficeIcon,
   BuildingLibraryIcon,
 } from "@heroicons/react/24/outline"
 import React from "react"
@@ -13,10 +14,10 @@ export default function NavLinks() {
   return (
     <div className="mt-6">
       <ALink name="Home" href="/home" icon={HomeIcon} />
-      <ALink name="Rooms" href="/rooms" icon={BuildingLibraryIcon} />
+      <ALink name="Rooms" href="/rooms" icon={BuildingOfficeIcon} />
       <ALink name="Tickets" href="/ticket" icon={TicketIcon} />
       <ALink name="Tour" href="/tour" icon={ShoppingBagIcon} />
-      <ALink name="Broadway" href="/broadway" icon={ShoppingBagIcon} />
+      <ALink name="Broadway" href="/broadway" icon={BuildingLibraryIcon} />
     </div>
   )
 }
