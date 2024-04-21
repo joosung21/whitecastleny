28f737e303bc9f4abc6398fdd6c515ea5eebd46f
Jay Ahn <joosung21@gmail.com>
2024-04-21 20:40:25 +0900

    구글계정 코드변경

--- src/pages/api/sheet.js
+++ src/pages/api/sheet.js
@@ -13,7 +13,8 @@ export default async function handler(req, res) {
 
   const sheets = google.sheets({ version: "v4", auth: client })
   // const spreadsheetId = "1ajGlvpYxCeZ7BD3zV2qrxnY-LWz6sUGgMR6fV68F3g0" // 스프레드시트 ID
-  const spreadsheetId = "13vecn8TGf2H_4G-U8Kwi-2vZGIbBYW3vgR8zu--QtXE" // 스프레드시트 ID 선주계정
+  // const spreadsheetId = "13vecn8TGf2H_4G-U8Kwi-2vZGIbBYW3vgR8zu--QtXE" // 스프레드시트 ID 선주계정
+  const spreadsheetId = "1uSpX-FNuTZP5faT6yhwFhiwwOhBakbUAGFBuaVqDi7A" // 스프레드시트 ID 티파니누나 계정
   const range = "Sheet1!A2:E10000" // 데이터를 불러올 범위
 
   try {
